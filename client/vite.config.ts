b9433903c92005bdba84c3967d079d06ca828b7a
[SW5] 예은선 <ysvln0912@gmail.com>
2024-02-22 20:32:15 +0900

    fix : backurl 변경

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const backUrl = "https://54.180.114.71";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -10,7 +11,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "https://today-movie.vercel.app/",
+        target: backUrl,
         changeOrigin: true,
       },
     },
