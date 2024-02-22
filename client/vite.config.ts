525d2580ffafa88aa9ec228942ba813782dc8e6e
[SW5] 예은선 <ysvln0912@gmail.com>
2024-02-22 21:50:18 +0900

    add : 배포 경로 테스트

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const backUrl = "https://54.180.114.71";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -11,7 +10,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: backUrl,
+        target: "https://54.180.114.71",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
