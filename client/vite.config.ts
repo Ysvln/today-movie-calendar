dbe43f8330f7381b2f866482f874338875863c78
[SW5] 예은선 <ysvln0912@gmail.com>
2024-02-25 15:18:40 +0900

    fix : port 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    port: 3000,
+    port: 80,
     open: true,
     host: true,
     proxy: {
