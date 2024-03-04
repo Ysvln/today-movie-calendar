3d50d081c7f062555c5a88fe8b0c0a336f3a5916
[SW5] 예은선 <ysvln0912@gmail.com>
2024-03-04 22:37:07 +0900

    comment : 타입 사용 이유 주석

--- client/src/hooks/useValidParams.ts
+++ client/src/hooks/useValidParams.ts
@@ -6,6 +6,7 @@ export const useValidParams = <
   const params = useParams<T>();
   //   console.log(params);
   //   {id : "3000"}
+  // key 값으로 오는 데이터가 불특정하기 때문에 Record Type으로 작성
   if (!params || Object.values(params).some((value) => value === undefined)) {
     throw new Error("유효하지 않은 값입니다.");
   }
