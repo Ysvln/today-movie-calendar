13c30e33ea4d309acb5600bf6e07ae1bd9b1d296
[SW5] 예은선 <ysvln0912@gmail.com>
2024-03-07 23:03:39 +0900

    refactor : input 컴포넌트 타입 확장

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -1,54 +1,21 @@
 import "./style.scss";
 
-import { ChangeEvent, forwardRef, Ref } from "react";
+import { InputHTMLAttributes, forwardRef, Ref } from "react";
 
 import ErrorText from "../ErrorText";
 
-interface InputProps {
-  type?: string;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  placeholder?: string;
-  name: string;
-  value?: string;
-  disabled?: boolean;
   error?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input = forwardRef(
-  (
-    {
-      type,
-      label,
-      placeholder,
-      name,
-      value,
-      error,
-      disabled,
-      onChange,
-    }: InputProps,
-    ref: Ref<HTMLInputElement>
-  ) => {
-    // const inputRef = useRef<HTMLInputElement>(null);
-    // useImperativeHandle(ref, () => ({
-    //   triggerFocus: () => {
-    //     inputRef.current?.focus();
-    //   },
-    // }));
+  ({ label, error, ...props }: InputProps, ref: Ref<HTMLInputElement>) => {
     return (
       <>
         {label && <label className="label">{label}</label>}
         <div className="input__container">
-          <input
-            ref={ref}
-            type={type}
-            placeholder={placeholder}
-            className={`input ${error && "error"}`}
-            name={name}
-            value={value}
-            disabled={disabled}
-            onChange={onChange}
-          />
+          <input ref={ref} className={`input ${error && "error"}`} {...props} />
           {error && <ErrorText>{error}</ErrorText>}
         </div>
       </>
