2b2bbed09a024449e7d4dcc418d39839b4a13288
[SW5] 예은선 <ysvln0912@gmail.com>
2024-03-07 23:08:25 +0900

    refactor : axios config 미적용 코드 삭제

--- client/src/apis/@config.ts
+++ client/src/apis/@config.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { AxiosResponse } from "axios";
+// import { AxiosResponse } from "axios";
 
 const apiBaseUrl = "/api";
 
@@ -8,36 +8,3 @@ export const api = axios.create({
   withCredentials: true,
   timeout: 5000,
 });
-
-type ApiFetcherParams = [string, any];
-export type ApiMethods = "get" | "post" | "put" | "delete" | "patch";
-export type APiFetcher = (...args: ApiFetcherParams) => Promise<any>;
-
-const getFetcher: (
-  path: string,
-  { params }: any
-) => Promise<AxiosResponse<any, any>> = async (path, params) => {
-  return await api.get(path, { params });
-};
-const postFetcher = async (path: string, body: any) => {
-  return await api.post(path, body);
-};
-const patchFetcher = async (path: string, body: any) => {
-  return await api.put(path, body);
-};
-const putFetcher = async (path: string, body: any) => {
-  return await api.put(path, body);
-};
-const deleteFetcher = async (path: string, params: any) => {
-  return await api.delete(path, { params });
-};
-
-// const args = {path:string, body: any } as const;
-
-export const API_FETCHER: { [key in ApiMethods]: APiFetcher } = {
-  get: (...args) => getFetcher(...args),
-  post: (...args) => postFetcher(...args),
-  put: (...args) => putFetcher(...args),
-  patch: (...args) => patchFetcher(...args),
-  delete: (...args) => deleteFetcher(...args),
-};
