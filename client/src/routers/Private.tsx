006e126ca1e4d104d808126ae78bbea76a26754c
[SW5] 예은선 <ysvln0912@gmail.com>
2024-03-08 01:51:05 +0900

    fix : cookie로 유저 확인

--- client/src/routers/Private.tsx
+++ client/src/routers/Private.tsx
@@ -5,7 +5,8 @@ import { useNavigate, useLocation } from "react-router-dom";
 import UserProvider from "@/provider/userProvider";
 import ScrollToTop from "@/components/ScrollToTop";
 import MESSAGE from "@/constants/message";
-import useCheckUserInfo from "@/hooks/useCheckUserInfo";
+import { getCookie } from "@/utils/cookie";
+// import useCheckUserInfo from "@/hooks/useCheckUserInfo";
 
 interface PrivateProps {
   children: ReactNode;
@@ -14,14 +15,14 @@ interface PrivateProps {
 const Private = ({ children }: PrivateProps) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const { user } = useCheckUserInfo();
 
   useEffect(() => {
-    if (!user.id) {
+    const isUser = getCookie("isUser");
+    if (isUser === "undefined") {
       alert(MESSAGE.LOGIN.REQUIRED);
       return navigate("/");
     }
-  }, [location.pathname, user]);
+  }, [location.pathname]);
 
   return (
     <UserProvider>
